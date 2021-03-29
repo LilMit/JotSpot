4123bf0f744162e2da83889d67d77ada64830820
Mittie Elaine Parr <parr.mi@northeastern.edu>
2021-03-29 14:58:09 -0400

    start project

---
+++ app/src/main/java/edu/northeastern/jotspot/EntryTypeSelection.java
@@ -0,0 +1,14 @@
+package edu.northeastern.jotspot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EntryTypeSelection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entry_type_selection);
+    }
+}
