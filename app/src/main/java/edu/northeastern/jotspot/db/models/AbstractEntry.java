86c4055e5ddf595baca2404cc556e5dce433e08d
Mittie Elaine Parr <parr.mi@northeastern.edu>
2021-04-07 13:25:42 -0400

    set up entry table

--- app/src/main/java/edu/northeastern/jotspot/db/models/AbstractEntry.java
+++ app/src/main/java/edu/northeastern/jotspot/db/models/AbstractEntry.java
@@ -1,13 +1,45 @@
 package edu.northeastern.jotspot.db.models;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import java.sql.Timestamp;
 
+@Entity(tableName = "entries")
 public abstract class AbstractEntry {
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name= "entryId")
+    private int id;
+
+    @ColumnInfo(name = "dateCreated")
     private Timestamp timestamp;
+
+    @ColumnInfo(name = "entryType")
     private EntryType type;
 
     public AbstractEntry(Timestamp timestamp, EntryType type) {
+        this.id = id;
         this.timestamp = timestamp;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NonNull int id) {
+        this.id = id;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setType(EntryType type) {
         this.type = type;
     }
 
