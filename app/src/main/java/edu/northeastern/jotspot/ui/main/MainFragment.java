6a3cfb09082d668ab5047a538ef9f8ad038d03fe
Mittie Elaine Parr <parr.mi@northeastern.edu>
2021-04-14 17:52:15 -0400

    work fragment into mainactivity, remove tutorial items that don't apply

--- app/src/main/java/edu/northeastern/jotspot/ui/main/MainFragment.java
+++ app/src/main/java/edu/northeastern/jotspot/ui/main/MainFragment.java
@@ -1,5 +1,6 @@
 package edu.northeastern.jotspot.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -22,58 +23,44 @@ import java.sql.Timestamp;
 import java.util.List;
 import java.util.Locale;
 
+import edu.northeastern.jotspot.EntryTypeSelection;
+import edu.northeastern.jotspot.MainActivity;
 import edu.northeastern.jotspot.R;
 import edu.northeastern.jotspot.db.models.Entry;
 import edu.northeastern.jotspot.db.models.EntryType;
 import edu.northeastern.jotspot.db.models.TextEntry;
 
 /**
- * This was created by following Chapter 68 of Android Studio 4.1 Development Essentials
+ * This was initially created by following Chapter 68 of Android Studio 4.1 Development Essentials then modified
  */
 public class MainFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     private MainViewModel mainViewModel;
     private EntryListAdapter adapter;
 
-    private TextView entryId;
-    private EditText entryTimestamp;
-    private EditText entryType;
+//    private TextView entryId;
+//    private TextView entryTimestamp;
+//    private TextView entryType;
 
     public MainFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainFragment newInstance(String param1, String param2) {
+    public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+////        if (getArguments() != null) {
+////            mParam1 = getArguments().getString(ARG_PARAM1);
+////            mParam2 = getArguments().getString(ARG_PARAM2);
+////        }
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -87,20 +74,20 @@ public class MainFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        entryId = getView().findViewById(R.id.entryId);
-        entryTimestamp = getView().findViewById(R.id.entryTimestamp);
-        entryType = getView().findViewById(R.id.entryType);
+//        entryId = getView().findViewById(R.id.entryId);
+//        entryTimestamp = getView().findViewById(R.id.entryTimestamp);
+//        entryType = getView().findViewById(R.id.entryType);
 
         listenerSetup();
         observerSetup();
         recyclerSetup();
     }
 
-    private void clearFields() {
-        entryId.setText("");
-        entryTimestamp.setText("");
-        entryType.setText("");
-    }
+//    private void clearFields() {
+//        entryId.setText("");
+//        entryTimestamp.setText("");
+//        entryType.setText("");
+//    }
 
     private void listenerSetup() {
         Button addButton = getView().findViewById(R.id.add_button);
@@ -109,60 +96,71 @@ public class MainFragment extends Fragment {
 
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                String timestamp = entryTimestamp.getText().toString();
-                String type = entryType.getText().toString();
+            public void onClick(View v) {
+                Intent i = new Intent(getActivity(), EntryTypeSelection.class);
+                startActivity(i);
+            }
+        });
+        //TODO reimplement in textEntryActivity and remove from here
 
-                if (!timestamp.equals("") && !type.equals("")) {
-                    Entry entry = new TextEntry(Timestamp.valueOf(timestamp));
-                    mainViewModel.insertEntry(entry);
-                    clearFields();
-                } else {
-                    entryId.setText("Incomplete information");
-                }
-            }
-        });
-        findButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mainViewModel.findEntry(entryTimestamp.getText().toString());
-            }
-        });
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mainViewModel.deleteEntry(entryId.getText().toString());
-                clearFields();
-            }
-        });
+//            @Override
+//            public void onClick(View view) {
+//                String timestamp = entryTimestamp.getText().toString();
+//                String type = entryType.getText().toString();
+//
+//                if (!timestamp.equals("") && !type.equals("")) {
+//                    Entry entry = new TextEntry(Timestamp.valueOf(timestamp));
+//                    mainViewModel.insertEntry(entry);
+//                    clearFields();
+//                } else {
+//                    entryId.setText("Incomplete information");
+//                }
+//            }
+//        });
+
+        //TODO reimplement
+//        findButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mainViewModel.findEntry(entryTimestamp.getText().toString());
+//            }
+//        });
+//        deleteButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mainViewModel.deleteEntry(entryId.getText().toString());
+//                clearFields();
+//            }
+//        });
 
     }
 
-    private void observerSetup(){
+    private void observerSetup() {
         mainViewModel.getAllEntries().observe(getViewLifecycleOwner(), new Observer<List<Entry>>() {
             @Override
             public void onChanged(List<Entry> entries) {
                 adapter.setEntryList(entries);
             }
         });
-        mainViewModel.getSearchResults().observe(getViewLifecycleOwner(), new Observer<List<Entry>>() {
-            @Override
-            public void onChanged(List<Entry> entries) {
-                if (entries.size() > 0){
-                    entryId.setText(String.format(Locale.US, "%d", entries.get(0).getId()));
-                    entryTimestamp.setText(entries.get(0).getTimestamp().toString());
-                    entryType.setText(String.format(Locale.US, "%d", entries.get(0).getType()));
-                } else {
-                    entryId.setText("No matching entries.");
-                }
-            }
-        });
+        //TODO reimplement in search later
+//        mainViewModel.getSearchResults().observe(getViewLifecycleOwner(), new Observer<List<Entry>>() {
+//            @Override
+//            public void onChanged(List<Entry> entries) {
+//                if (entries.size() > 0) {
+//                    entryId.setText(String.format(Locale.US, "%d", entries.get(0).getId()));
+//                    entryTimestamp.setText(entries.get(0).getTimestamp().toString());
+//                    entryType.setText(String.format(Locale.US, "%d", entries.get(0).getType()));
+//                } else {
+//                    entryId.setText("No matching entries.");
+//                }
+//            }
+//        });
     }
 
-    private void recyclerSetup(){
+    private void recyclerSetup() {
         RecyclerView recyclerView;
         adapter = new EntryListAdapter(R.layout.entry_list_item);
-        recyclerView=getView().findViewById(R.id.entry_recycler);
+        recyclerView = getView().findViewById(R.id.entry_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
     }
