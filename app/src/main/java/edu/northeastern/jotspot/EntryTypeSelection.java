0f38d6ead74a674c837337997308db7114d0f43e
Mittie Elaine Parr <parr.mi@northeastern.edu>
2021-04-16 16:59:43 -0400

    start adding audio recording entry

--- app/src/main/java/edu/northeastern/jotspot/EntryTypeSelection.java
+++ app/src/main/java/edu/northeastern/jotspot/EntryTypeSelection.java
@@ -7,17 +7,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import edu.northeastern.jotspot.newEntry.AudioEntryActivity;
 import edu.northeastern.jotspot.newEntry.TextEntryActivity;
 
 public class EntryTypeSelection extends AppCompatActivity {
 
     private ImageButton textEntryButton;
+    private ImageButton audioEntryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry_type_selection);
         textEntryButton = findViewById(R.id.select_text_entry);
+        audioEntryButton = findViewById(R.id.audio_entry_button);
 
         textEntryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +30,15 @@ public class EntryTypeSelection extends AppCompatActivity {
                 finish();
             }
         });
+
+        audioEntryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(EntryTypeSelection.this, AudioEntryActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/edu/northeastern/jotspot/newEntry/AudioEntryActivity.java
@@ -0,0 +1,16 @@
+package edu.northeastern.jotspot.newEntry;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.northeastern.jotspot.R;
+
+public class AudioEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_entry);
+    }
+}
